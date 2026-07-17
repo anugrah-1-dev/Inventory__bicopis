2e2fd977a1567c6ce77d2cf85d1826abf8f5ab3e
anugrah-1-dev <tejomaliki1@gmail.com>
2026-07-17 18:21:46 +0700

    first commit

--- public/build/assets/Button-BzNhGUQB.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-C2tYEVDb.js";function c({className:r="",children:n,colorScheme:a="primary",icon:o,...s}){const t={primary:" primary ",secondary:" secondary ",warning:" warning ",danger:" danger "};return e.jsxs("button",{className:"btn"+t[a]+r,...s,children:[o,n]})}export{c as B};
